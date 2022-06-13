797ae729f20c52acfcc9d41a8cc3fbf1e013e5fc
FranciscoFranco79 <franciscorubenfrancoperez@gmail.com>
2022-06-13 16:51:10 +0100

    porcentaje a la rama

--- src/main/java/calculadora_FRPE.java
+++ src/main/java/calculadora_FRPE.java
@@ -8,12 +8,18 @@
  * @author ceti
  */
 public class calculadora_FRPE {
+    
+    public static int porcentaje (int a, int b){
+        return a%b;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("El porcentaje es: " + porcentaje(10,100));
     }
     
 }
