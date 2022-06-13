7434a5c312a23b7b669707f7e7414b00f1cd866e
FranciscoFranco79 <franciscorubenfrancoperez@gmail.com>
2022-06-13 15:29:08 +0100

    Mult y Div a la rama

--- src/main/java/calculadora_FRPE.java
+++ src/main/java/calculadora_FRPE.java
@@ -8,12 +8,22 @@
  * @author ceti
  */
 public class calculadora_FRPE {
+    
+    public static int Multiplicacion(int a, int b) {
+        return a*b;
+    }
+    public static int Division(int a, int b) {
+        return a/b;
+    }
 
-    /**
+    /**f
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("La multiplicacion es: "+ Multiplicacion(2,4));
+        System.out.println("La division es: "+ Division(2,4));
     }
     
 }
