3d894ac9ebf717bdfa9747ebd3fd2c94f8aa1aab
FranciscoFranco79 <franciscorubenfrancoperez@gmail.com>
2022-06-15 15:38:03 +0100

    suma modificada a la rama

--- src/main/java/calculadora_FRPE.java
+++ src/main/java/calculadora_FRPE.java
@@ -8,12 +8,20 @@
  * @author ceti
  */
 public class calculadora_FRPE {
+    
+    public static int Suma_modificada(int a, int b, int c){
+        
+        return c+b+a;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("La suma total es: " + Suma_modificada(3,4,5));
+        
     }
-    
+   
 }
